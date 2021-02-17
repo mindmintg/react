6fa63117e3e5f79090bf7a9d6c40e2ea5463c35a
sasithorn <mint251710@gmail.com>
2021-02-17 12:46:16 +0700

    react

---
+++ react/test/src/Appheader.js
@@ -0,0 +1,12 @@
+function Appheader() {
+    
+    return (
+       
+          <header className="App-header">
+              <img className ="header-logo" src ="/logo.jpg"/>
+    
+           
+          </header>
+          );
+          }
+    export default Appheader;
---
+++ react/test/src/items.js
@@ -0,0 +1,20 @@
+const items =[
+    {
+      title :"cat",
+      thumbnaiUrl :"/d1.jpg"
+      
+    
+    },
+    {
+      title :"cat2",
+      thumbnaiUrl :"/d2.jpg"
+    },
+    {
+      title :"cat3",
+      thumbnaiUrl :"/d3.png"
+      
+    
+    }
+  ];
+export default items;
+
